c481cacc218b040f755506f78763ff535c6e05c7
Jdaie <490844515@qq.com>
2025-11-27 15:41:08 +0800

    feat: print aplay -l at start

--- example/run_test.sh
+++ example/run_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# print aplay -l list output
+aplay -l
+
 # Find the sound card index for wm8960soundcard
 card_index=$(awk '/wm8960soundcard/ {print $1}' /proc/asound/cards | head -n1)
 # Default to 1 if not found
